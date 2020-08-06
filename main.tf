6c67d63806ffd3d2b86fb4abfd572ca34a6c16fe
Andrew Pelletier <andrew.pelletier@connexta.com>
2020-08-06 11:41:00 -0700

    commented out module

--- main.tf
+++ main.tf
@@ -5,23 +5,23 @@ provider "aws" {
   secret_key = var.AWS_SECRET_KEY
 }
 
-module "high_altitude" {
-  source  = "terraform.cie.vi2e.io/High-Altitude/high-altitude/aws"
-  version = "~>1.0.12"
-  providers = {
-    aws = aws.gov-west
-  }
-  app_tpl_filepath_filename = "templates/app.tpl"
-  app_version = "1.25.0"
-  application_name = "andrew-soaesb"
-  aws_ami = "ami-adecdbcc"
-  http_proxy = "internal-transit-s-rELBProx-PEHFQP9VW7UJ-653953035.us-gov-west-1.elb.amazonaws.com:3128/"
-  private_subnets = ["subnet-ce6c82a9", "subnet-c60e739f", "subnet-fcda2fb5"]
-  public_subnets = ["subnet-c97e90ae", "subnet-0037c249", "subnet-c404799d"]
-  vpc_id = "vpc-30686a54"
-  NEXUS_USERNAME = "${var.NEXUS_USERNAME}"
-  NEXUS_PASSWORD = "${var.NEXUS_PASSWORD}"
-}
+//module "high_altitude" {
+//  source  = "terraform.cie.vi2e.io/High-Altitude/high-altitude/aws"
+//  version = "~>1.0.12"
+//  providers = {
+//    aws = aws.gov-west
+//  }
+//  app_tpl_filepath_filename = "templates/app.tpl"
+//  app_version = "1.25.0"
+//  application_name = "andrew-soaesb"
+//  aws_ami = "ami-adecdbcc"
+//  http_proxy = "internal-transit-s-rELBProx-PEHFQP9VW7UJ-653953035.us-gov-west-1.elb.amazonaws.com:3128/"
+//  private_subnets = ["subnet-ce6c82a9", "subnet-c60e739f", "subnet-fcda2fb5"]
+//  public_subnets = ["subnet-c97e90ae", "subnet-0037c249", "subnet-c404799d"]
+//  vpc_id = "vpc-30686a54"
+//  NEXUS_USERNAME = "${var.NEXUS_USERNAME}"
+//  NEXUS_PASSWORD = "${var.NEXUS_PASSWORD}"
+//}
 
 # subnet-ce6c82a9
 ## subnet-c60e739f
