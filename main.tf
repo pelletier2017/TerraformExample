8028d2450e5e440a26d2c169041b8f2c944a56ee
Andrew Pelletier <andrew.pelletier@connexta.com>
2020-08-06 11:39:26 -0700

    add public subnets

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ module "high_altitude" {
   app_version = "1.25.0"
   application_name = "andrew-soaesb"
   aws_ami = "ami-adecdbcc"
-  http_proxy = "internal-transit-s-rELBProx-PEHFQP9VW7UJ-653953035.us-gov-west-1.elb.amazonaws.com:3128"
+  http_proxy = "internal-transit-s-rELBProx-PEHFQP9VW7UJ-653953035.us-gov-west-1.elb.amazonaws.com:3128/"
   private_subnets = ["subnet-ce6c82a9", "subnet-c60e739f", "subnet-fcda2fb5"]
-  public_subnets = []
+  public_subnets = ["subnet-c97e90ae", "subnet-0037c249", "subnet-c404799d"]
   vpc_id = "vpc-30686a54"
   NEXUS_USERNAME = "${var.NEXUS_USERNAME}"
   NEXUS_PASSWORD = "${var.NEXUS_PASSWORD}"
