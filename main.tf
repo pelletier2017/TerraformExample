2e533c47d6128e4a4091f6144904c7814e9caa32
Andrew Pelletier <andrew.pelletier@connexta.com>
2020-08-06 10:21:13 -0700

    add subnet id

--- main.tf
+++ main.tf
@@ -4,11 +4,13 @@ provider "aws" {
   secret_key = var.AWS_SECRET_KEY
 }
 
-
+// subnet-ce6c82a9
+// subnet-c60e739f
+// subnet-fcda2fb5
 resource "aws_instance" "app" {
   ami = "ami-adecdbcc"
   instance_type = "t2.micro"
-  
+  subnet_id = "subnet-ce6c82a9"
   tags = {
     Name = "andrews app"
   }
